0083a1ae86291533320454840fd4b3e9e586ba9d
Marcin <marcin.adam.mroczko@gmail.com>
2020-11-01 14:43:35 +0100

    add before element

--- src/components/FetchRandomMovies/styles.js
+++ src/components/FetchRandomMovies/styles.js
@@ -18,7 +18,7 @@ export const MoviesListEl = styled.li`
   margin-top: 1rem;
   list-style-type: none;
   background-color: var(--color-black);
-  trasnition: 0.5s opacity ease;
+  transition: 0.5s opacity ease-in-out;
   :hover {
     cursor: pointer;
     .sc-gzVnrw {
@@ -30,11 +30,19 @@ export const MoviesListEl = styled.li`
     }
     p {
       color: white;
+      border-color: tomato;
       z-index: 1000;
+      ::before {
+        opacity: 1;
+      }
+    }
+    img {
+      filter: grayscale(100%);
     }
   }
   img {
     z-index: var(--over-content);
+    transition: filter 0.35s ease-in-out-in-out;
   }
 `
 
@@ -43,9 +51,9 @@ export const Title = styled.h2`
   position: absolute;
   left: 13rem;
   z-index: var(--over-content);
-  color: red;
+  color: tomato;
   font-size: 1.2rem;
-  transition: 0.35s ease;
+  transition: 0.35s ease-in-out;
   transition-property: opacity, transform;
   will-change: transform;
   span {
@@ -62,7 +70,7 @@ export const VideoWrap = styled.div`
   height: 17rem;
   position: relative;
   left: -71%;
-  transition: 0.5s ease;
+  transition: 0.5s ease-in-out;
   transitiom-property: transform;
   will-cahnge: transform;
   video {
@@ -79,20 +87,35 @@ export const VideoWrap = styled.div`
 export const MediaWrap = styled.div`
   display: flex;
   overflow: hidden;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
 `
 
 export const MovieDesc = styled.p`
   min-height: 17rem;
   position: absolute;
   top: 100%;
-  padding: 2rem 2rem 2rem 2rem;
+  padding: 2rem;
   margin-top: 0;
   z-index: -1;
   font-size: 1.6rem;
+  border-top: 1px solid transparent;
   background-color: var(--color-black);
   color: var(--color-white);
   color: black;
-  border-top: 1px solid rgba(255, 255, 255, 0.1);
-  transition: 0.5s ease;
-  transition-property: transform, color, background-color;
+  transition: 0.5s ease-in-out;
+  transition-property: transform, color, background-color, border-color;
+  ::before {
+    content: "";
+    height: 0;
+    position: absolute;
+    top: -15px;
+    left: 13%;
+    opacity: 0;
+    width: 0;
+    height: 0;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-bottom: 15px solid tomato;
+    transition: 0.5s opacity ease-in-out;
+  }
 `
