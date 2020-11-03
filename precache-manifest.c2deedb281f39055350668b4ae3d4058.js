d5f1ea6e4470b9df3a684253c6fd602a8cfe6ce7
Marcin <marcin.adam.mroczko@gmail.com>
2020-11-03 11:00:12 +0100

    Updates

---
+++ precache-manifest.c2deedb281f39055350668b4ae3d4058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3230034686c8c9b68f047ef97217552b",
+    "url": "/iTunes-top-100-movies/index.html"
+  },
+  {
+    "revision": "fa8ad1b92094c67ce031",
+    "url": "/iTunes-top-100-movies/static/js/2.63f2522d.chunk.js"
+  },
+  {
+    "revision": "8826fb38371a95349e3f",
+    "url": "/iTunes-top-100-movies/static/js/main.20516ce0.chunk.js"
+  },
+  {
+    "revision": "40d6610010f02dedea2b",
+    "url": "/iTunes-top-100-movies/static/js/runtime-main.4bb605aa.js"
+  },
+  {
+    "revision": "f2fa4b6a6e4c77621f2d8052e7575f2c",
+    "url": "/iTunes-top-100-movies/static/media/logo.f2fa4b6a.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/iTunes-top-100-movies/precache-manifest.c2deedb281f39055350668b4ae3d4058.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iTunes-top-100-movies/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
