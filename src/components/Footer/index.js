76fc68d146303c4e0febd09821fece33e8b9f358
Marcin <marcin.adam.mroczko@gmail.com>
2020-11-12 21:39:17 +0100

    tv screen fade

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Root, TV, BrokenScreen } from "./styles"
+import sal from "sal.js"
+import "sal.js/dist/sal.css"
 
-export const Footer = () => (
-  <Root>
-    <TV>
-      <BrokenScreen></BrokenScreen>
-    </TV>
-    <p>&copy; 2020 Pandemia still spreads out ;(</p>
-  </Root>
-)
+export const Footer = () => {
+  useEffect(sal, [])
+  return (
+    <Root>
+      <TV>
+        <BrokenScreen data-sal="fade" data-sal-delay="500"></BrokenScreen>
+      </TV>
+      <p>&copy; 2020 Pandemia still spreads out ;(</p>
+    </Root>
+  )
+}
